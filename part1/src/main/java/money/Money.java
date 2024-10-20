c80ee839632d04cdb29ea077d5e1e86397e42899
ytgw <tagawa0920@gmail.com>
2024-10-20 14:21:41 +0000

    DollarのFactoryメソッドを追加

--- part1/src/main/java/money/Money.java
+++ part1/src/main/java/money/Money.java
@@ -2,10 +2,16 @@ package money;
 
 public class Money {
     protected int amount;
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         boolean isSameClass = this.getClass().equals(money.getClass());
         boolean isSameAmount = this.amount == money.amount;
+
         return isSameClass && isSameAmount;
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
     }
 }
