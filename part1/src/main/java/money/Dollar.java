44a7f5eabd752b2964523ce8ae96236eaab18722
ytgw <tagawa0920@gmail.com>
2024-10-20 14:58:27 +0000

    DollarもFrancと同様に修正

--- part1/src/main/java/money/Dollar.java
+++ part1/src/main/java/money/Dollar.java
@@ -1,12 +1,12 @@
 package money;
 
 class Dollar extends Money {
-    Dollar(int amount) {
+    Dollar(int amount, String currency) {
         this.amount = amount;
-        this.currency = "USD";
+        this.currency = currency;
     }
 
     Money times(int multiplier) {
-        return new Dollar(this.amount * multiplier);
+        return Money.dollar(this.amount * multiplier);
     }
 }
