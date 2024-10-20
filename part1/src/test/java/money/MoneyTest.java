baac57087f7dc21ab244fbc4129e9b956fcadfaf
ytgw <tagawa0920@gmail.com>
2024-10-20 14:30:24 +0000

    テストコードをFactoryメソッドを使うように変更

--- part1/src/test/java/money/MoneyTest.java
+++ part1/src/test/java/money/MoneyTest.java
@@ -9,23 +9,23 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
         // Dollar
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(3)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(3)));
 
         // Franc
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(3)));
 
         // Dollar and Franc
-        assertFalse(new Dollar(5).equals(new Franc(5)));
-        assertFalse(new Dollar(5).equals(new Franc(3)));
+        assertFalse(Money.dollar(5).equals(new Franc(5)));
+        assertFalse(Money.dollar(5).equals(new Franc(3)));
     }
 
     @Test
