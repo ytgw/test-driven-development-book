ee7b53fbfaf7be759f3a1530bf6e9dd4fd808654
ytgw <tagawa0920@gmail.com>
2024-10-20 10:03:03 +0000

    timesメソッドのテストコードの追加

---
+++ part1/src/test/java/money/MoneyTest.java
@@ -0,0 +1,13 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
