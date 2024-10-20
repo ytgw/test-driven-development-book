5c27e616acb18d10ed1ba3ba22df84a89a4602bf
ytgw <tagawa0920@gmail.com>
2024-10-20 13:45:04 +0000

    Francのequalsメソッドを親クラスに移行

--- part1/src/main/java/money/Franc.java
+++ part1/src/main/java/money/Franc.java
@@ -7,8 +7,4 @@ class Franc extends Money {
     Franc times(int multiplier) {
         return new Franc(this.amount * multiplier);
     }
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return this.amount == franc.amount;
-    }
 }
