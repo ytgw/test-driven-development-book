5126e2e3525715fd018335f9723d5fdaad37f3f4
ytgw <tagawa0920@gmail.com>
2024-10-20 10:15:56 +0000

    Dollarクラスのリファクタリング

--- part1/src/main/java/money/Dollar.java
+++ part1/src/main/java/money/Dollar.java
@@ -1,7 +1,11 @@
 package money;
 
 class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}
-    void times(int multiplier) {}    
+    int amount;
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+    void times(int multiplier) {
+        this.amount *= multiplier;
+    }    
 }
