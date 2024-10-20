383f619a7c6fa48412e776fee0852ea3c1159e21
ytgw <tagawa0920@gmail.com>
2024-10-20 14:47:06 +0000

    currencyメソッド共通化の準備

--- part1/src/main/java/money/Dollar.java
+++ part1/src/main/java/money/Dollar.java
@@ -1,12 +1,15 @@
 package money;
 
 class Dollar extends Money {
+    private String currency;
+
     Dollar(int amount) {
         this.amount = amount;
+        this.currency = "USD";
     }
 
     String currency() {
-        return "USD";
+        return currency;
     }
 
     Money times(int multiplier) {
--- part1/src/main/java/money/Franc.java
+++ part1/src/main/java/money/Franc.java
@@ -1,12 +1,15 @@
 package money;
 
 class Franc extends Money {
+    private String currency;
+
     Franc(int amount) {
         this.amount = amount;
+        this.currency = "CHF";
     }
 
     String currency() {
-        return "CHF";
+        return currency;
     }
 
     Money times(int multiplier) {
