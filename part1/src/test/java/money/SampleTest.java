c03109fbb37cfce3542849c6362959aba6729bb0
ytgw <tagawa0920@gmail.com>
2024-10-20 09:51:24 +0000

    サンプルのテストコードを追加

---
+++ part1/src/test/java/money/SampleTest.java
@@ -0,0 +1,11 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+    @Test
+    public void testEqual() {
+        assertEquals(2, 2);
+    }
+}
