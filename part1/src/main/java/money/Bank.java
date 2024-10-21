3c7ce971101e103f8976ccf41877c9afe2918838
ytgw <tagawa0920@gmail.com>
2024-10-21 14:40:24 +0000

    Expressionインターフェースにreduceメソッドを引き上げる準備

--- part1/src/main/java/money/Bank.java
+++ part1/src/main/java/money/Bank.java
@@ -2,7 +2,7 @@ package money;
 
 class Bank {
     Money reduce(Expression source, String to) {
-        if (source instanceof Money) return (Money) source;
+        if (source instanceof Money) return ((Money) source).reduce(to);
     
         Sum sum = (Sum) source;
         return sum.reduce(to);
