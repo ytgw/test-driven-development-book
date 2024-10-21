85072eeea1a0aed092c3663ccbb3289d0b760666
ytgw <tagawa0920@gmail.com>
2024-10-21 14:19:20 +0000

    testPlusReturnsSumを追加

--- part1/src/test/java/money/MoneyTest.java
+++ part1/src/test/java/money/MoneyTest.java
@@ -35,4 +35,13 @@ public class MoneyTest {
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(8), reduced);
     }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(Money.dollar(3));
+        Sum sum = (Sum) result;
+        assertEquals(Money.dollar(5), sum.augend);
+        assertEquals(Money.dollar(3), sum.addend);
+    }
 }
