5ed41516972f435127f69e733035a7cc07735caa
ytgw <tagawa0920@gmail.com>
2024-10-25 06:58:20 +0000

    親クラスにname属性を引き上げ

--- part2/xunit.py
+++ part2/xunit.py
@@ -1,12 +1,17 @@
 class TestCase:
-    pass
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+
 
 class WasRun(TestCase):
     wasRun: bool
 
     def __init__(self, name: str):
         self.wasRun = False
-        self.name = name
+        super().__init__(name)
 
     def run(self):
         method = getattr(self, self.name)
