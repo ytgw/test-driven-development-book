dd884377fe1bf4de47b1ce2165546b2399b8a690
ytgw <tagawa0920@gmail.com>
2024-10-25 06:49:01 +0000

    testMethodがないエラーを修正

--- part2/xunit.py
+++ part2/xunit.py
@@ -4,6 +4,9 @@ class WasRun:
     def __init__(self, name: str):
         self.wasRun = False
 
+    def testMethod(self):
+        pass
+
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
