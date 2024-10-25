cdf44e13dfd4364aa833f299e0e403623dc23168
ytgw <tagawa0920@gmail.com>
2024-10-25 06:52:13 +0000

    テスト対象メソッドをrun経由で呼ぶ

--- part2/xunit.py
+++ part2/xunit.py
@@ -4,8 +4,12 @@ class WasRun:
     def __init__(self, name: str):
         self.wasRun = False
 
+    def run(self):
+        self.testMethod()
+
     def testMethod(self):
         self.wasRun = True
+
 
 test = WasRun("testMethod")
 print(test.wasRun)
